7800fb9a15e6b7e864131b65256daf2a1042e665
Sarawut Pe <mr.sarawutpe@gmail.com>
2023-07-13 22:31:18 +0700

    .

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+var cookieParser = require('cookie-parser')
 
+const port = 3000;
+
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  console.log(req.headers);
+
+  console.log("token is", req.cookies.token)
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
